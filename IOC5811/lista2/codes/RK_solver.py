bd404ab2688831e85d6e8fdec170b05d9cbf9ee1
Danilo A Silva <nilodna@gmail.com>
2017-07-20 22:06:58 -0300

    joblab 20Jul

--- IOC5811/lista2/codes/RK_solver.py
+++ IOC5811/lista2/codes/RK_solver.py
@@ -20,7 +20,80 @@ Some useful sources and links:
 import math
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
 
+matplotlib.style.use("ggplot")
+
+
+def importMatfile(fname=''):
+
+    import scipy.io as sio
+
+    fname = '../outputs/mats/'+fname
+
+    data = sio.loadmat(fname)
+
+    cr  = np.squeeze(data['cr'])
+    sig = np.squeeze(data['sig'])
+    P   = data['P']
+
+    return cr, sig, P
+
+def plotar_tudo(k,y,cr,sig,Pphasemax, Pmax, figname, title):
+    """ 
+        plotar 4 subplots with:
+            cr x k*1000
+            sig x k*1000
+
+            Pmax x 1e-3*y
+            Pphasemax x 1e-3*y
+    """
+
+    kplot = k*1000
+    yplot = y*1e-3
+
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(10,10))
+
+    # plot cr x k
+    axes[0, 0].plot(kplot, cr)
+    axes[0, 0].set_title(u'Velocidade de Fase - $c_r$')
+    axes[0, 0].set_xlabel(u'phase speeds [$m s^{-1}$]')
+    axes[0, 0].set_ylabel(u'Número de onda [$km^{-1}$]')
+    axes[0, 0].set_xlim([0.003, 0.04])
+    axes[0, 0].set_ylim([0., 0.05])
+    axes[0, 0].text(0.0360052, 0.00467125, '(A)')
+
+    axes[0, 1].plot(kplot, sig)
+    axes[0, 1].set_title(u'Taxa de Crescimento - $\sigma = k c_i$')
+    axes[0, 1].set_xlabel(u'growth rates [$days^{-1}$]')
+    axes[0, 1].set_ylabel(u'Número de onda [$km^{-1}$]')
+    axes[0, 1].set_ylim([0, 0.02])
+    axes[0, 1].set_xlim([0.003, 0.04])
+    axes[0, 1].text(0.0362016, 0.0019526, '(B)')
+
+    axes[1, 0].plot(Pmax, yplot)
+    axes[1, 0].set_title(u'Amplitude do Modo mais Instável')
+    axes[1, 0].set_xlabel(u'Amplitude do Modo P/ $\phi$')
+    axes[1, 0].set_ylabel(u'Distância Meridional [km]')
+    axes[1, 0].set_ylim([-L*1e-3, L*1e-3])
+    axes[1, 0].set_xlim([0., 0.18])
+
+    axes[1, 0].text(0.165055, -173.761, '(C)')
+
+    axes[1, 1].plot(Pphasemax, yplot)
+    axes[1, 1].set_title('Fase')
+    axes[1, 1].set_xlabel(u'Fase [$^o$]')
+    axes[1, 1].set_ylabel(u'Distância Meridional [km]')    
+    axes[1, 1].set_ylim([-L*1e-3, L*1e-3])
+    axes[1, 1].set_xlim([-70, 0])
+    axes[1, 1].text(-6.60426, 180.489, '(D)')
+
+    plt.suptitle(u'Solução da Equação de Rayleigh-Kuo - ' + title, fontsize=20)
+
+    if figname!='': # então rola salvar
+        plt.savefig('../outputs/'+figname, dpi=100)
+    else:
+        plt.show()
 
 
 ### SET GLOBAL VARIABLES
@@ -30,7 +103,7 @@ beta = (2 * 7.29e-5 * math.cos(lat * np.pi/180))/6371e3
 
 L  = 200e3 # 1/2 domain
 dy = 2e3
-y  = np.arange(-L, L, dy)
+y  = np.arange(-L, L+dy, dy)
 ny = len(y)
 
 # jet profile
@@ -42,18 +115,18 @@ Uyy= np.gradient(Uy, dy)
 
 Qy = beta - Uyy
 
-# plotar
-fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True)
+# # plotar
+# fig, (ax1, ax2) = plt.subplots(nrows=1, ncols=2, sharey=True)
 
-ax1.plot(U, 1e-3*y, 'b')
-ax1.set_xlabel('jet velocity in m/s')
-ax1.set_ylabel('meridional distance in m')
+# ax1.plot(U, 1e-3*y, 'b')
+# ax1.set_xlabel('jet velocity in m/s')
+# ax1.set_ylabel('meridional distance in m')
 
-ax2.plot(Qy, 1e-3*y, 'g')
-ax2.axvline(0, color='black')
-ax2.set_xlabel('potential vorticity gradiente in 1/s')
-ax2.set_ylabel('meridional distance in m')
-plt.show()
+# ax2.plot(Qy, 1e-3*y, 'g')
+# ax2.axvline(0, color='black')
+# ax2.set_xlabel('potential vorticity gradiente in 1/s')
+# ax2.set_ylabel('meridional distance in m')
+# plt.show()
 
 #### THE EIGENVALUE PROBLEM
 
@@ -66,7 +139,7 @@ Problema:
         A - matriz de dados
         B - matriz identidade
         c - autovalor associado
-        P - autovetor de A
+        P - autovetor de A $P \neq 0$
 
     A e B são matrizes tridiagonais, ou seja, matrizes em que os elementos
     não nulos são da diagonal principal e 1 diagonal acima e 1 abaixo da diagonal
@@ -101,30 +174,62 @@ cr = np.zeros(nk)
 sig = cr
 P = np.zeros([ny,nk])
 
-for n in range(0,nk,1): # wavenumber loop
+# for n in range(0,nk,1): # wavenumber loop
 
-    # setting up the matrix coefficients
-    L1 = 1/dy/dy
-    L2 = 2/dy/dy + k[n]*k[n]
-    L3 = L1
+#     # setting up the matrix coefficients
+#     L1 = 1/dy/dy
+#     L2 = 2/dy/dy + k[n]*k[n]
+#     L3 = L1
 
-    # building tridiagonal matrices A and B
-    A = np.diag(Qy - L2 * U) + np.diag( L3 * U[1:ny],1) + np.diag(L1*U[1:ny] ,-1)
-    B = np.diag(-L2 * np.squeeze(np.ones([1,ny]))) + np.diag(L3*np.squeeze(np.ones([1,ny-1])), 1) + np.diag(L1*np.squeeze(np.ones([1,ny-1])), -1)
-    # obtaining the eigenvalue matrix
-    C = np.linalg.lstsq(B, A)[0]
+#     # building tridiagonal matrices A and B
+#     A = np.diag(Qy - L2 * U) + np.diag( L3 * U[1:ny],1) + np.diag(L1*U[1:ny] ,-1)
+#     B = np.diag(-L2 * np.squeeze(np.ones([1,ny]))) + np.diag(L3*np.squeeze(np.ones([1,ny-1])), 1) + np.diag(L1*np.squeeze(np.ones([1,ny-1])), -1)
+#     # obtaining the eigenvalue matrix
+#     C = np.linalg.lstsq(B, A)
 
-    # calculate eigenvalues and eigenvector for k[n]
-    lamb, F  = np.linalg.eig(C)
-    w = np.diag(lamb) # create matrix [ny,ny]
-    ci = np.nanmax(np.diag(np.imag(w)))
+#     # calculate eigenvalues and eigenvector for k[n]
+#     # return evals/lambda, evecs/F
+#     lamb, F  = np.linalg.eig(C[0])
+#     # extract the max imaginary part
+#     ci = np.nanmax(np.diag(np.imag(lamb)))
 
-    if ci == 0:
-        sig[n] = 0
-        cr[n]  = np.nan
-        P[:,n] = np.nan*np.ones([ny])
-    else:
-        sig[n] = k[n]*ci*86400          # growth rate in 1/days
-        cr[n]  = np.real(w[jmax,jmax])  # phase speed in m/s
-        P[:,n] = F[:, jmax]             # meridional mode structure
+#     if ci == 0:
+#         sig[n] = 0
+#         cr[n]  = np.nan
+#         P[:,n] = np.nan*np.ones([ny])
+#     else:
+#         # find the index of ci
+#         jmax = np.where(np.imag(lamb) == ci)
+#         sig[n] = k[n]*ci*86400          # growth rate in 1/days
+#         cr[n]  = np.real(lamb[jmax,jmax])  # phase speed in m/s
+#         P[:,n] = F[:, jmax]             # meridional mode structure
 
+exercs = ['exF.mat', 'exG.mat', 'exH.mat']
+
+for ex in exercs:
+    print("Plotando dados do %s" % (ex))
+    # import data
+    cr, sig, P = importMatfile(fname=ex)
+
+    Pamp   = np.abs(P)
+    Pphase = 180/np.pi * np.angle(P) # np.angle retorna o angulo do argumento complexo
+
+    # finding and normalizing most unstable mode
+    imax   = np.where(sig == np.nanmax(sig))[0]
+    sigmax = sig[imax]
+
+    Pmax      = np.squeeze(Pamp[:, imax])
+    
+    # orthonomalization
+    Pmax      = Pmax / (np.linalg.norm(Pmax))
+    Pphasemax = Pphase[:, imax]
+
+    plotar_tudo(k, y, cr, sig, Pphasemax, Pmax, figname=ex.replace('.mat', '.png'), title='Ex %s'%(ex[2:3]))
+
+
+import glob
+import os
+
+lstFile = glob.glob('../outputs/ex*')
+for fname in lstFile:
+    os.system('convert -trim %s %s' % (fname, fname))
