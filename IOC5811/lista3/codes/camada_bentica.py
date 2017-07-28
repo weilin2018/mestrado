6c52f0ff92dee0114427baa3baff4a24a6b2fdc3
Danilo A. Silva <nilodna@gmail.com>
2017-07-28 19:21:21 -0300

    joblab_28Jul

--- IOC5811/lista3/codes/camada_bentica.py
+++ IOC5811/lista3/codes/camada_bentica.py
@@ -1,1 +1,17 @@
 """ https://ocefpaf.github.io/python4oceanographers/blog/2015/02/09/compass/"""
+
+
+
+
+
+
+"""
+Funções:
+
+
+    def exec2() - plotar hodógrafo
+    def exec3() - plotar perfil
+    def exec6() - perfil de distribuição meridional
+        do bombeamento de ekman bentico
+
+"""
