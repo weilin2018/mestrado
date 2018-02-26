b1b1985f013e6ac288333e94b385e00830569188
Danilo A. Silva <nilodna@gmail.com>
2018-02-26 18:09:26 -0300

    new routines

---
+++ artigoTG/rotinas/fig1_studyarea.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python2
+#-*-coding:utf-8-*-
+
+import numpy as np # atribuir as arrays do .cdf para numpy's arrays
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from matplotlib.patches import Polygon
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset, inset_axes
+import cmocean as cmo
+
+import pickle
+
+def draw_square(lats,lons,m):
+    x,y=m(lons,lats)
+    xy=zip(x,y)
+    poly = Polygon(xy,facecolor='blue', alpha=0.4, edgecolor='black')
+    plt.gca().add_patch(poly)
+
+# definindo coordenadas para plotar a região desejada
+llon = -44.9;
+ulon = -43.45;
+
+llat = -23.6;
+ulat = -22.75;
+
+######### DETALHES PARA PLOTAR
+CNAAAbase  = [-44.460789, -23.006711]
+ilhagrande = [-44.613300, -23.141362]#-23.137504, -44.613300
+sepetiba   = [-43.864138, -23.003667]
+piraquaraf = [-44.441523, -23.011983]
+itaorna    = [-44.472463, -23.006116]#-23.017116,
+porOeste   = [-44.508007, -23.194821]
+porLeste   = [-44.113577, -23.070955]
+canCentral = [-44.333599, -23.083153]
+
+marambaia     = [-43.989471, -23.083472]
+mangaratiba   = [-44.056691, -22.944938]
+paraty        = [-44.757599, -23.232831]
+angradosreis  = [-44.358047, -23.020794]
+
+# tarituba      = [-44.598025, -23.039031]
+## pontos de descarga/captação
+fozMambucaba  = [-44.521247, -23.026884]
+descargaCNAAA = [-44.445079, -23.012130]
+captacaoCNAAA = [-44.459445, -23.009400]
+
+# séries temporais
+
+loc_w210 = [[-23.010962, -44.440706], [-22.9741811,-44.323831], [-23.012479, -44.307350], [-23.059381, -44.241407], [-22.994208,-44.039795],[-23.088036,-44.098059],[-23.096075,-44.010663]]
+nam_w210 = ['Piraquara de Fora','Angra dos Reis (A)', 'Angra dos Reis (B)', 'Canal Central', 'Mangaratiba','Porção Leste (BIG)', 'Marambaia']
+
+loc_w50 = [[-23.010962, -44.440706], [-22.960510, -44.394883],[-22.9741811,-44.323831],[-23.034976, -44.525694],[-23.213933, -44.704401]]
+nam_w50 = ['Piraquara de Fora','Ilha Comprida', 'Angra dos Reis (A)', 'Rio Mambucada', 'Paraty']
+
+loc_wcptec = [[-23.010962, -44.440706], [-22.960510, -44.394883],[-22.9741811,-44.323831],[-23.012479, -44.307350], [-23.059381, -44.241407], [-22.994208,-44.039795],[-23.088036,-44.098059],[-23.096075,-44.010663]]
+nam_wcptec = ['Piraquara de Fora', 'Ilha Comprida', 'Angra dos Reis (A)', 'Angra dos Reis (B)', 'Canal Central', 'Porção Leste', 'Mangaratiba', 'Marambaia']
+
+
+### quadrado
+lat0 = -22.980121
+lat1 = -23.054853
+lon0 = -44.374144
+lon1 = -44.558726
+
+fig = plt.figure(figsize=(20,15))
+ax = fig.add_subplot(111)
+
+# Plotando a base do mapa
+# m=Basemap(projection='merc',llcrnrlat=llat,urcrnrlat=ulat,llcrnrlon=llon,urcrnrlon=ulon,resolution='f')
+
+# pra agilizar o processo, puxa-se um mapa já feito e salvo em pickle
+m=pickle.load(open("bigmap.p","rb"))
+
+# plotar outras coisas do mapa
+m.drawcoastlines(linewidth=0.4) #linha de costa em alta resolução
+m.drawmapboundary(fill_color='#e5f2ff') # fill_color colore o oceano
+m.fillcontinents(color='#ffd480') # colorir o continente
+
+# definir meridianos e paralelos para plotar no mapa
+meridians=np.arange(-44.9,-33.4,0.25)
+parallels=np.arange(-23.6,-22.75,0.1)
+# desenhar meridianos e paralelos conforme definido acima
+m.drawparallels(parallels,labels=[True,False,False,True],fontsize=13,fontweight='bold',color='gray')
+m.drawmeridians(meridians,labels=[True,False,False,True],fontsize=13,fontweight='bold',color='gray')
+
+# plotar detalhes
+x,y=m(CNAAAbase[0], CNAAAbase[0])
+m.plot(x,y, 'bo', markersize=14)
+
+x,y=m(ilhagrande[0],ilhagrande[1])
+ax.text(x,y,u'Ilha Grande\n Bay',color='#000000', fontsize=15)
+
+x,y=m(sepetiba[0],sepetiba[1])
+ax.text(x,y,u'Sepetiba\n Bay',color='#000000', fontsize=15)
+
+x,y=m(porOeste[0],porOeste[1])
+ax.text(x,y,u'West \n Portion',color='#800000', fontsize=12)
+
+x,y=m(porLeste[0],porLeste[1])
+ax.text(x,y,u'East \n Portion',color='#800000', fontsize=12)
+
+x,y=m(canCentral[0],canCentral[1])
+ax.text(x,y,u'Central Channel',color='#800000', fontsize=12)
+
+
+#### desenhar quadrado
+lats = [lat0,lat1,lat1,lat0]
+lons = [lon0,lon1,lon1,lon0]
+draw_square(lats,lons,m)
+
+loc_w210 = [[-23.010962, -44.440706], [-22.9741811,-44.323831], [-23.012479, -44.307350], [-23.059381, -44.241407], [-22.994208,-44.039795],[-23.088036,-44.098059],[-23.096075,-44.010663]]
+nam_w210 = ['Piraquara de Fora','Angra dos Reis (A)', 'Angra dos Reis (B)', 'Canal Central', 'Mangaratiba','Porção Leste (BIG)', 'Marambaia']
+
+loc_w50 = [[-22.960510, -44.394883],[-23.034976, -44.525694],[-23.213933, -44.704401]]
+nam_w50 = ['Ilha Comprida', 'Rio Mambucada', 'Paraty']
+
+x,y = m(loc_w210[0][1],loc_w210[0][0])
+plt.scatter(x,y,marker='*',color='black',s=100,label=nam_w210[0])
+
+x,y = m(loc_w210[1][1],loc_w210[1][0])
+plt.scatter(x,y,marker='*',color='fuchsia',s=100,label=nam_w210[1])
+
+x,y = m(loc_w210[2][1],loc_w210[2][0])
+plt.scatter(x,y,marker='*',color='sienna',s=100,label=nam_w210[2])
+
+x,y = m(loc_w210[3][1],loc_w210[3][0])
+plt.scatter(x,y,marker='*',color='indigo',s=100,label=nam_w210[3])
+
+x,y = m(loc_w210[4][1],loc_w210[4][0])
+plt.scatter(x,y,marker='*',color='black',s=100,label=nam_w210[4])
+
+x,y = m(loc_w210[5][1],loc_w210[5][0])
+plt.scatter(x,y,marker='*',color='tan',s=100,label='Porcao Leste (BIG)')
+
+x,y = m(loc_w210[6][1],loc_w210[6][0])
+plt.scatter(x,y,marker='*',color='darkmagenta',s=100,label=nam_w210[6])
+
+
+x,y = m(loc_w50[0][1],loc_w50[0][0])
+plt.scatter(x,y,marker='*',color='lightcoral',s=100,label=nam_w50[0])
+
+x,y = m(loc_w50[1][1],loc_w50[1][0])
+plt.scatter(x,y,marker='*',color='darkorange',s=100,label=nam_w50[1])
+
+x,y = m(loc_w50[2][1],loc_w50[2][0])
+plt.scatter(x,y,marker='*',color='seagreen',s=100,label=nam_w50[2])
+
+# create legend with semi transparent background
+lg = plt.legend(loc='upper left',fontsize=12,numpoints=1,scatterpoints=1)
+lg.get_frame().set_alpha(.4)
+
+
+plt.show()
---
+++ artigoTG/rotinas/figs4_6_dispersion.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python2
+#-*-coding:utf-8-*-
+'''
+Rotina para plotar dispersão da pluma de trítio integrada na coluna em 4 instantes
+
+PLOTA AS 4 IMAGENS EM UMA MESMA FIGURA
+
+Cenário w50 e w210
+
+'''
+
+%reset -f
+#################################################
+#   MÓDULOS IMPORTAÇÃO/EXPORTAÇÃO ARQUIVOS      #
+#################################################
+import xray as xr # ler netcdf
+from Tkinter import Tk
+from tkFileDialog import askopenfilename,askdirectory
+import pickle
+
+#################################################
+#           MÓDULOS GRÁFICOS                    #
+#################################################
+import matplotlib.pyplot as plt
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset, inset_axes
+import cmocean as cmo
+import matplotlib.ticker as ticker
+# from mpl_toolkits.axes_grid1 import make_axes_locatable
+import matplotlib
+
+
+#################################################
+#           MÓDULOS CIENTÍFICOS                 #
+#################################################
+import scipy.io as sio
+import numpy as np
+import pandas as pd
+
+#################################################
+#           MÓDULOS MISCELANEOUS                #
+#################################################
+import os
+import re
+import sys
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+#### Criando o mapa da região
+def make_map(location='bigmap.p'):
+    # definindo coordenadas para plotar a região desejada
+    # llon = -44.9;
+    # ulon = -43.45;
+    # llat = -23.6;
+    # ulat = -22.75;
+    fig, ax = plt.subplots(figsize=(12,8))
+
+    # m=Basemap(projection='merc',llcrnrlat=llat,urcrnrlat=ulat,llcrnrlon=llon,urcrnrlon=ulon,resolution='f')
+    # pra agilizar o processo, puxa-se um mapa já feito e salvo em pickle
+    m=pickle.load(open("/media/danilo/Danilo/TG/from_tripoli/rotinas/"+location,"rb"))
+
+    m.ax = ax
+    return fig, m
+
+def make_minimap():
+    # create small region figure
+
+    m2=pickle.load(open("/media/danilo/Danilo/TG/from_tripoli/rotinas/smallmap.p","rb"))
+
+    # #### plotar batimetria
+    # lon3,lat3=m2(lon,lat)
+    # contorno = np.arange(-80,0,0.1)
+    # m2.contourf(lon3,lat3,-depth,contorno,cmap=cmo.cm.bathy)
+
+    # plotar outras coisas do mapa
+    m2.drawcoastlines(linewidth=0.4) #linha de costa em alta resolução
+    #m2.drawmapboundary(fill_color='#e5f2ff') # fill_color colore o oceano
+    m2.fillcontinents(color='#D8D8D8',lake_color='#99ffff') # colorir o continente
+
+    return m2
+
+
+def extract_data():
+    # entra no diretório em que o código foi rodado
+    os.chdir(directory)
+    # pede para usuário selecionar o arquivo que deseja carregar
+    filename = askopenfilename()
+    # carrega o arquivo para a variável f_xr
+    f_xr = xr.open_dataset(filename)
+    # extrai informações importantes (longitude, latitude e tempo)
+    lon = f_xr['lon'].data
+    lat = f_xr['lat'].data
+    time = f_xr['time'].data
+    ## ======== RETIRANDO LAT E LON IGUAL A 0 ====
+
+    ind=np.where(lon==0)
+    lon[ind]=np.nan
+    ind=np.where(lat==0)
+    lat[ind]=np.nan
+
+    return lon,lat,time,f_xr
+
+
+def convertData2Bq(c,unit='mg/L'):
+
+    # constante = 0.3e+22
+    c[c<0.0] = np.nan # valores negativos são removidos
+    if unit == 'mg/L':
+        # converter de mg para g
+        c = c*.1e-3
+    else:
+        # converter de kg para g
+        c = c*.1e3
+
+    c = c*35334440479135.016 # converter de g para Bq
+
+    return c
+
+################################################################################################################################################################
+#                                           PROGRAMA PRINCIPAL                                                                                                 #
+################################################################################################################################################################
+
+# determina qual o diretório atual (muito importante para não precisar rodar a rotina somente da pasta em que ela está)
+Tk().withdraw() # we don't want a full GUI, so keep the root window from appearing
+directory = os.getcwd()
+outputDir = askdirectory(initialdir='/media/danilo/Danilo/TG/article_tg/data/simulacoes/',title='Selecione o diretório para salvar as imagens:')
+
+# selecionando o titulo fixo dos plots
+# txt = 'Dispersão de $^{3}H$ forçado pelo:\n'+ r'Vento (de NE $5 m s^{-1}$), Maré (TPXO) e Descarga Fluvial - '
+txt = ['Dispersão da pluma de $^{3}H$ na superfície - Após %s horas do vazamento']
+# txt = selectTitle(directory)
+
+#### extrair dados
+lon,lat,time,f_xr = extract_data()
+
+# extrair concentracao, mas tomando a soma da coluna e não apenas superficial
+concInt = f_xr['conc'].data.sum(axis=1)
+
+# remover f_xr pra liberar espaço na memória
+del f_xr
+
+# converter o dado de Kg para Bq/l
+k=convertData2Bq(concInt)
+# liberar espaço na memória
+# del c
+# normalizar os valores
+k=k/.1e+7
+
+# linhas de contorno para usar no contourf
+maxC =np.nanmax(k[-1,:,:])+45
+contour_levels=np.arange(0.,150,maxC/1e+3)
+
+# contour_levels=np.arange(0.,0.01,0.00005)
+
+max_img = time.shape[0] - 1
+cont_time=np.arange(0,max_img+1,1)
+
+# definir o vetor de dia: de 0 a 10 dias a cada 10./120 tempo (a cada 2 horas aproximadamente)
+# timeD = np.arange(0,10,10./120)
+t0    = pd.to_datetime(str(time[0])).strftime('%H')
+t1    = pd.to_datetime(str(time[1])).strftime('%H')
+dtImg = int(t1) - int(t0)
+timeH = np.arange(dtImg,(time.shape[0])*dtImg+dtImg,dtImg)
+
+################################################################################################################################################################
+################################################################################################################################################################
+################################################################################################################################################################
+
+
+def plotar_dado(fig,ax,k1,tmpDecor,horas,cbPlot=False,location='bigmap.p'):
+    #### plotar dados
+    #fig,m=make_map(location=location)
+    m=pickle.load(open("/media/danilo/Danilo/TG/from_tripoli/rotinas/"+location,"rb"))
+    m.ax = ax
+
+    lon2,lat2=m(lon,lat)
+
+    m.drawcoastlines(linewidth=.4)
+    m.fillcontinents(color='#D8D8D8',lake_color='#99ffff')
+
+    # title_out = txt+datetime2title(time[i])
+    # plt.title(txt,fontsize=15)
+
+    parallels=np.arange(-23.6,-22.75,0.2)
+    m.drawcoastlines(linewidth=0.4)
+    m.drawparallels(parallels,labels=[1,0,0,1],linewidth=0.1,fontsize=10)
+    meridians=np.arange(-44.9,-33.4,0.5)
+    m.drawmeridians(meridians,labels=[1,0,0,1],linewidth=0.1,fontsize=10)
+    # plotando as informações de dispersão do material radioativo
+    pc=m.contourf(lon2,lat2,k1[:,:],contour_levels,cmap=cmo.cm.matter,extend='both')
+
+    if cbPlot:
+        # definir quantidade de marcadores da colorbar - muito util para valores com muitos zeros =)
+        # ticks_cb  = np.linspace(contour_levels[0],contour_levels[-1],7,endpoint=True)
+        ticks_cb  = [0,30,60,90,120,150]
+
+        #  ordem do add_axes [left, bottom, width, height]
+        cbaxes = fig.add_axes([0.335, 0.05, 0.36, 0.02])
+
+        # plotar a colorbar, usando os ticks estabelecidos e o tamanho definido acima.
+        cb=plt.colorbar(pc,orientation='horizontal',ticks=ticks_cb,format='%.1d',cax=cbaxes)
+        # plotar a legenda da colorbar
+        cb.set_label('Integrated Concentratino (x$10^{7}Bq.m^{-3}$)',fontsize=8)
+        cb.ax.tick_params(labelsize=8)
+
+    #########################################################
+    #              Plotar dados dentro do mapa              #
+    # #######################################################
+    # ### datetime
+    # tempo = '%s horas após \n vazamento' % txt
+    # plt.text(.18,.75,tempo,ha='center',va='center',transform=m.ax.transAxes,fontsize=20)
+    if horas:
+        tempo = 'After \n %s hours'%(tmpDecor)
+        plt.text(0.9,0.85,tempo,ha='center',va='center',transform=m.ax.transAxes,fontsize=10)
+    else:
+        tempo = 'After \n %s days'%(tmpDecor)
+        plt.text(0.9,0.85,tempo,ha='center',va='center',transform=m.ax.transAxes,fontsize=10)
+
+    #########################################################
+    #          Plotar mapa detalhado da região              #
+    #########################################################
+    axins = zoomed_inset_axes(m.ax, 2.5, loc=4)
+    axins.set_xlim(-10,0)
+    axins.set_ylim(3,10)
+
+    plt.xticks(visible=False)
+    plt.yticks(visible=False)
+
+    # create small figure of detailed region
+    m2=make_minimap()
+
+    #### plotar dado no mapa pequeno
+    lon2,lat2=m2(lon,lat)
+    pc=m2.contourf(lon2,lat2,k1[:,:],contour_levels,cmap=cmo.cm.matter,extend='both')
+
+    plt.subplots_adjust(hspace=0.10) # ajustar a distancia vertical entre os gráficos pra ficar mais compacto
+
+
+
+os.system('clear')
+
+# 2 horas, 12 horas, 3 dias, 7 dias
+print('EVOLUÇÃO DA PLUMA ... Plotando imagens para 0, 30 e 60 dias')
+# instantes = [0,39,119,159,199,239]
+instantes = [0,120,-1]
+
+fig,ax = plt.subplots(nrows=3,ncols=1,figsize=(10,20))
+
+#def plotar_dado(fig,ax,k1,location='bigmap.p',cbPlot=False):
+k1 = k[instantes[0],:,:]
+plotar_dado(fig,ax[0],k1,tmpDecor=str(timeH[instantes[0]]),horas=True,cbPlot=True)
+
+k1 = k[instantes[1],:,:]
+plotar_dado(fig,ax[1],k1,tmpDecor=str(timeH[instantes[1]]/24),horas=False,cbPlot=False)
+
+k1 = k[instantes[2],:,:]
+plotar_dado(fig,ax[2],k1,tmpDecor=str(timeH[instantes[2]]/24),horas=False,cbPlot=False)
+
+# k1 = k[instantes[3],:,:]
+# plotar_dado(fig,ax[1][1],k1,tmpDecor=str(timeH[instantes[3]]/24),horas=False,cbPlot=False)
+
+
+plt.show()
