96cf5cd4fb5a4ffbcdd6b9cee3dc4912e4bd57bc
Danilo A. Silva <nilodna@gmail.com>
2018-03-23 17:22:27 -0300

    upload routine to cut grb2 file with cdo

--- masterThesis_analysis/routines/cut_CSFR_fromFTP.py
+++ masterThesis_analysis/routines/cut_CSFR_fromFTP.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python
 
-''' 
+'''
 script para utilizar o cdo (Climate Data Operators) para recortar os arquivos grb2 que tenho
 para uma região específica
+
+
 '''
 
 import os
@@ -15,13 +17,28 @@ sys.path.append('masterThesisPack/')
 import masterThesisPack as oceano
 
 BASE_DIR = oceano.make_dir()
-DATA_DIR = BASE_DIR.replace('github/', 'ventopcse/data/CFSR_FTP/')
-SAVE_DIR = BASE_DIR.replace('github/', 'ventopcse/data/CFSR_FTP/recortados/')
+DATA_DIR = BASE_DIR.replace('github/', 'ventopcse/data/FTP/')
+SAVE_DIR = BASE_DIR.replace('github/', 'ventopcse/data/FTP/recortados/')
 
-nfiles = glob.glob(DATA_DIR+'*.grb2')
+nfiles = glob.glob(DATA_DIR+'*.grib2')
 nfiles.sort()
+
+os.system('clear')
 
 for f in nfiles:
 	print("Cutting: %s"%(f.split('/')[-1]))
-	os.system('cdo -sellonlatbox,-50,-40,-20,-30 %s %s'%(f, f.replace('CFSR_FTP/','CFSR_FTP/recortados/')))
+	os.system('cdo -sellonlatbox,-50,-40,-20,-30 %s %s'%(f, f.replace('FTP/','FTP/recortados/')))
+	os.system('mv %s %s'%(f, f.replace('FTP/','FTP/processados/')))
 
+# apos recortar, converter os arquivos para netcdf usando o proprio cdo
+
+nfiles = glob.glob(DATA_DIR.replace('FTP/','FTP/recortados/*.grib2'))
+nfiles.sort()
+
+os.system('clear')
+os.system('Converting grib2 files to netCDF files')
+
+for f in nfiles:
+	os.system('cdo -f nc copy %s %s' % (f, f.replace('.grib2', '.nc')))
+
+os.system('mv %s %s' % (SAVE_DIR+'*.grib2', SAVE_DIR+'grb2/'))
