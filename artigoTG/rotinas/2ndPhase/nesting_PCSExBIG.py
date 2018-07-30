1e125f6364c8851ba209b8e9def11ad6d106347f
Danilo A. Silva <nilodna@gmail.com>
2018-07-30 02:21:42 -0300

    trying nesting grids

--- artigoTG/rotinas/2ndPhase/nesting_PCSExBIG.py
+++ artigoTG/rotinas/2ndPhase/nesting_PCSExBIG.py
@@ -68,23 +68,30 @@ def find_nearest(lon,lat,ilon,ilat,k=2):
     for p in pontos:
         ind.append(np.where(lon == p[1]))
 
-    ind = np.asarray(ind)[0]
+    ind = np.asarray(ind)
 
     # vetores para separar i e j para facilitar resgatar os dados de concentração
     iss=[]
     jss=[]
 
-    # se tiver retornado mais de um ponto, entao ind tera um shape > 1
-    if ind.ndim > 2:
-        for dim in range(ind.shape[0]):
-            for i,j in ind[dim]:
-                iss.append(int(i))
-                jss.append(int(j))
-    else:
-        for i,j in ind:
-            iss.append(int(i))
-            jss.append(int(j))
+    # if k > 1
+    if k > 1:
+        for dim in range(k):
+            iss.append(int(ind[dim][0]))
+            jss.append(int(ind[dim][1]))
 
+
+    # # se tiver retornado mais de um ponto, entao ind tera um shape > 1
+    # if ind.ndim > 2:
+    #     for dim in range(ind.shape[0]):
+    #         for i,j in ind[dim]:
+    #             iss.append(int(i))
+    #             jss.append(int(j))
+    # else:
+    #     for i,j in ind:
+    #         iss.append(int(i))
+    #         jss.append(int(j))
+    #
     return iss,jss
 
 def extract_Coordinates(ncin):
@@ -155,14 +162,121 @@ def viewGrid(lon,lat):
     m.plot(lon.T,lat.T,'k',latlon=True)
 
     plt.show()
+
+def findNearest(lonBnd,latBnd,k=1):
+
+    iss = []
+    jss = []
+
+    for lonref,latref in zip(lonBnd,latBnd):
+        lo = lon_coarsed.ravel()
+        la = lat_coarsed.ravel()
+
+        coords = []
+
+        for i,j in zip(la,lo):
+            coords.append([i,j])
+
+        coords = np.array(coords)
+
+        locations_posi = [[latref,lonref]]
+
+        locs = np.asarray(locations_posi)
+
+        tree = cKDTree(coords)
+        # procura em tree os pontos mais próximos dos pontos definidos acima
+        dists,indexes = tree.query(locs,k=k)
+
+        # searching coordinates based on the indexes
+        pontos = [coords[index] for index in indexes]
+        pontos = np.squeeze(np.asarray(pontos))
+
+        # findind indexes from lat and lon
+        if pontos.ndim == 1: # if there is only 1 point:
+            ind = np.where(lon_coarsed == pontos[1])
+        else: # for multiples points
+            ind = [np.where(lon_coarsed == p[1]) for p in pontos]
+        # convert list to np.ndarray
+        ind = np.squeeze(np.asarray(ind))
+
+        # vetores para separar i e j para facilitar resgatar os dados de concentração
+        if ind.ndim == 1:
+            iss.append(int(ind[0]))
+            jss.append(int(ind[1]))
+        elif ind.ndim >= 2:
+            # iss = []
+            # jss = []
+
+            dims = ind.ndim
+            for dim in range(dims):
+                iss.append(ind[dim][0])
+                jss.append(ind[dim][1])
+
+    return iss,jss
+
+
+def find(lon_coarsed,lat_coarsed,lonBnd,latBnd):
+    """Find nearest points between coarsed and refined grids.
+
+    Parameters
+    ----------
+    lon_coarsed : numpy.ndarray
+        Longitude of coarsed grid.
+    lat_coarsed : numpy.ndarray
+        Latitude of coarsed grid
+    lonBnd : numpy.ndarray
+        Boundaries longitude of refined grid.
+    latBnd : numpy.ndarray
+        Boundaries latitudes of refined grid.
+
+    Returns
+    -------
+    iss,jss : list
+        List with indexes of coarsed grid, containing all
+        nearest points to boundaries locations from refined grid.
+
+    """
+
+    # removing nan points from coarsed grid
+    indNan_coarsed = np.where(~np.isnan(lon_coarsed))
+    x,y = lon_coarsed[indNan_coarsed], lat_coarsed[indNan_coarsed]
+    # removing from refined grid
+    indNan = np.where(~np.isnan(lonBnd))
+    xi,yi = lonBnd[indNan],latBnd[indNan]
+
+    # creating array with coordinates
+    mdgrid = np.array(zip( x.ravel(),y.ravel() ))
+    # instancing KDTree object
+    kdt = kd.KDTree(mdgrid)
+
+    # locating points (distances and indexes)
+    mdi_dist, mdi = kdt.query(np.array(zip(xi,yi)))
+
+    # locating coordinates based on indexes in mdi
+    points = np.asarray([mdgrid[index] for index in mdi])
+
+    # based on points, find Is and Js
+    ind    = np.asarray([np.where(lon_coarsed == p[0]) for p in points])
+
+    # classifying all point found, separating in iss and jss, with same size
+    for index in ind:
+        if index[-1].shape == 1:
+            iss.append(int(index[0]))
+            jss.append(int(index[1]))
+        else:
+            iss.append(index[0][-1])
+            jss.append(index[1][-1])
+
+    return iss,jss
+
 ##############################################################################
 #                               MAIN CODE                                    #
 ##############################################################################
 # beginnig of the main code
-
+BASE_DIR   = '/media/danilo/Danilo/mestrado/artigo_data/simulacoes/2ndPhase/'
 # define PCSE file and BIG file
-fname_pcse = '/media/danilo/Danilo/mestrado/ventopcse/output/cut_exp06.cdf'
-fname_big  = '/media/danilo/Danilo/mestrado/artigo_data/simulacoes/2ndPhase/run01_validation/run01_validation.cdf'
+fname_pcse = BASE_DIR + "pcse_elevation.cdf"
+fname_big  = BASE_DIR + "reRUN00.cdf"
 
 # read netcdf file
 ncin_pcse  = xr.open_dataset(fname_pcse)
@@ -178,6 +292,65 @@ lon_refined,lat_refined = extract_Coordinates(ncin_big)
 # visualizing grid big
 # viewGrid(lon_refined,lat_refined)
 
-# visualize both grid and defining boundaries, in coordinates, for
-# big model_grid
-westBC,eastBC,southBC = visualize_grids(ncin_pcse,ncin_big)
+# select boundaries for refined grid
+westBC  = [lon_refined[2,:],lat_refined[2,:]]
+eastBC  = [lon_refined[-2,:],lat_refined[-2,:]]
+southBC = [lon_refined[:,-2],lat_refined[:,-2]]
+
+boundaries = np.asarray([westBC,southBC,eastBC])
+
+#### --------------- finding nearest points of all boundaries created
+
+# finding nearest points to the west boundary
+i = 0# for west boundary
+lonBnd,latBnd = boundaries[i][0],boundaries[i][1]
+
+iss_west,jss_west = find(lon_coarsed,lat_coarsed,lonBnd,latBnd)
+
+# finding nearest points to the south boundary
+i = 1# for south boundary
+lonBnd,latBnd = boundaries[i][0],boundaries[i][1]
+
+iss_south,jss_south = find(lon_coarsed,lat_coarsed,lonBnd,latBnd)
+
+# finding nearest points to the east boundary
+i = 2# for east boundary
+lonBnd,latBnd = boundaries[i][0],boundaries[i][1]
+
+iss_east,jss_east = find(lon_coarsed,lat_coarsed,lonBnd,latBnd)
+
+#### --------------- visualize both grids together
+offset = 0.25                # offset for coordinates
+
+fig,ax = plt.subplots()
+m = oceano.make_map(ax,llat=np.nanmin(lat_refined)-offset, ulat=np.nanmax(lat_refined), llon=np.nanmin(lon_refined)-offset, ulon=np.nanmax(lon_refined)+offset,resolution='i')
+
+m.plot(lon_coarsed,lat_coarsed,'k',latlon=True,alpha=.3);
+m.plot(lon_coarsed.T,lat_coarsed.T,'k',latlon=True,alpha=.3);
+
+# m.plot(lon_refined,lat_refined,'r',latlon=True,alpha=.3);
+# m.plot(lon_refined.T,lat_refined.T,'r',latlon=True,alpha=.3);
+
+# plotting boundaries of refined grid
+os.system('clear')
+for i in range(3):
+    print('####################### Boundary: %i'%(i))
+    lonBnd = boundaries[i][0]
+    latBnd = boundaries[i][1]
+    # m.plot(lonBnd,latBnd,'r',latlon=True,alpha=.9)
+    m.scatter(lonBnd,latBnd,s=30,c='r',latlon=True)
+
+# plotting in m
+# first west boundary
+for i,j in zip(iss_west,jss_west):
+    m.scatter(lon_coarsed[i,j],lat_coarsed[i,j],s=50,c='g',latlon=True)
+
+# first south boundary
+for i,j in zip(iss_south,jss_south):
+    m.scatter(lon_coarsed[i,j],lat_coarsed[i,j],s=50,c='g',latlon=True)
+
+# first east boundary
+for i,j in zip(iss_east,jss_east):
+    m.scatter(lon_coarsed[i,j],lat_coarsed[i,j],s=50,c='g',latlon=True)
+
+#### --------------- loading data from pcse_gcmplt.cdf and creating file
