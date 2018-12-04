9af25985fe80724c21c9e797205e3428969c09c3
Danilo A. Silva <nilodna@gmail.com>
2018-12-04 08:33:39 -0200

    reading cebimar data routines and figures

--- masterThesis_analysis/routines/araca/read_cebimar.py
+++ masterThesis_analysis/routines/araca/read_cebimar.py
@@ -14,6 +14,8 @@ from mpl_toolkits.axes_grid1 import make_axes_locatable
 from matplotlib import dates
 import datetime
 
+from dateutil import parser
+
 import matplotlib
 matplotlib.style.use('ggplot')
 
@@ -22,11 +24,42 @@ sys.path.append('masterThesisPack/')
 
 import masterThesisPack as oceano
 
-##############################################################################
-#                          [GEN] FUNCTIONS                                   #
-##############################################################################
-# insert functions here
+def createDates(dates,times):
 
+    date = []
+
+    for d,h in zip(dates,times):
+        t = d + ' ' + h
+        date.append(parser.parse(t))
+
+    return date
+
+def verticalProfile_structure(nrows,ncols,figsize,xlim):
+
+    # criando figura
+    fig,axes = plt.subplots(nrows=nrows,ncols=ncols,figsize=figsize)
+
+    # customizacoes para um perfil vertical em subplots
+    axes[0,0].set_ylabel('Profundidade [m]',fontsize=8)
+    axes[1,0].set_ylabel('Profundidade [m]',fontsize=8)
+
+    # removendo ticklabels da esquerda
+    axes[0,1].tick_params(axis='y',labelleft='off') # remove ticklabels
+    axes[0,2].tick_params(axis='y',labelleft='off') # remove ticklabels
+    axes[1,1].tick_params(axis='y',labelleft='off') # remove ticklabels
+    axes[1,2].tick_params(axis='y',labelleft='off') # remove ticklabels
+
+    # x-axis on the top
+    for i in range(nrows):
+        for j in range(ncols):
+            axes[i,j].invert_yaxis()
+            axes[i,j].xaxis.tick_top()
+            axes[i,j].set_xlim(xlim)
+            axes[i,j].tick_params(axis='both',which='major',labelsize=8)
+
+            # axes[i,j].set_ylim([0,-15])
+
+    return fig,axes
 
 ##############################################################################
 #                               MAIN CODE                                    #
@@ -37,8 +70,91 @@ DATA_DIR = BASE_DIR.replace('github','ventopcse/data/Araca/Aurea_CEBIMAR')
 
 fname = DATA_DIR + 'casts_bined_4Dott.txt'
 
-ncin = pd.read_csv(fname,sep='	')
-ncin.drop(['Type','Station'],inplace=True,axis=1) # removendo algumas colunas
-ncin.set_index('Cruise',inplace=True) # definindo index
+df = pd.read_csv(fname,sep='	')
+df.drop(['Type','Station'],inplace=True,axis=1) # removendo algumas colunas
+df.set_index('Cruise',inplace=True) # definindo index
 
-# pensar numa forma de agrupar pelo Cruise
+# trocando o nome das colunas para algo mais simples
+df.columns = ['Date','Time','Lon','Lat','Depth','Temperature','Salinity']
+
+# converter date e time em datetime para podermos usar como condicao de separacao
+dates = df.Date.values
+times = df.Time.values
+
+# removendo as antigas colunas de tempo
+df.drop(['Date','Time'],inplace=True,axis=1)
+
+date = createDates(dates,times)
+
+# inserindo as novas datas no dataframe
+df['datetime'] = date
+
+# selecionando somente os perfis de Jan/2014 (designados pelo Cruise/index 201401)
+jan2014 = df.loc[201401]
+
+# Selecionando os dados pelo cruzeiro realizado em cada dia e hora
+# indexes = jan2014.datetime
+# indexes = indexes.drop_duplicates()
+# nPerfis = len(indexes) # quantidade de perfis obtidos
+
+# agrupando dados
+d = jan2014.drop(['Lon','Lat'],axis=1)
+d.set_index('Depth',inplace=True)
+
+# armazenando em caso de necessidade
+oldDepths = d.index.values
+
+grouped = d.groupby('datetime')
+
+dct = {}
+
+for name,group in grouped:
+    dct[name] = group
+
+keys = dct.keys()
+keys.sort()
+
+# plotando
+fig,ax = verticalProfile_structure(nrows=2,ncols=3,figsize=figsize,xlim=[23,30])
+plt.suptitle(u'Perfis Verticais no Canal de São Sebastião (CEBIMAR)',fontsize=10)
+
+ax[0,0].plot(dct[keys[0]].Temperature.values,dct[keys[0]].index.values,label=keys[0])
+ax[0,0].text(25.,1.5,str(keys[0]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+ax[0,1].plot(dct[keys[1]].Temperature.values,dct[keys[1]].index.values,label=keys[1])
+ax[0,1].text(25.,1.5,str(keys[1]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+ax[0,2].plot(dct[keys[2]].Temperature.values,dct[keys[2]].index.values,label=keys[2])
+ax[0,2].text(25.,1.5,str(keys[2]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+ax[1,0].plot(dct[keys[3]].Temperature.values,dct[keys[3]].index.values,label=keys[3])
+ax[1,0].text(25.,1.5,str(keys[3]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+ax[1,1].plot(dct[keys[4]].Temperature.values,dct[keys[4]].index.values,label=keys[4])
+ax[1,1].text(25.,1.5,str(keys[4]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+ax[1,2].plot(dct[keys[5]].Temperature.values,dct[keys[5]].index.values,label=keys[5])
+ax[1,2].text(25.,1.5,str(keys[5]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+
+plt.savefig(BASE_DIR+'masterThesis_analysis/figures/dados_observados/cebimar_temperatura.eps')
+
+
+
+# plotando
+fig,ax = verticalProfile_structure(nrows=2,ncols=3,figsize=figsize,xlim=[34,36])
+plt.suptitle(u'Perfis Verticais no Canal de São Sebastião (CEBIMAR)',fontsize=10)
+
+ax[0,0].plot(dct[keys[0]].Salinity.values,dct[keys[0]].index.values,label=keys[0])
+ax[0,0].text(35.5,1.5,str(keys[0]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+
+ax[0,1].plot(dct[keys[1]].Salinity.values,dct[keys[1]].index.values,label=keys[1])
+ax[0,1].text(35.5,1.5,str(keys[1]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+
+ax[0,2].plot(dct[keys[2]].Salinity.values,dct[keys[2]].index.values,label=keys[2])
+ax[0,2].text(35.5,1.5,str(keys[2]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+
+ax[1,0].plot(dct[keys[3]].Salinity.values,dct[keys[3]].index.values,label=keys[3])
+ax[1,0].text(35.5,1.5,str(keys[3]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+
+ax[1,1].plot(dct[keys[4]].Salinity.values,dct[keys[4]].index.values,label=keys[4])
+ax[1,1].text(35.5,1.5,str(keys[4]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+
+ax[1,2].plot(dct[keys[5]].Salinity.values,dct[keys[5]].index.values,label=keys[5])
+ax[1,2].text(35.5,1.5,str(keys[5]).replace(' ','\n'),horizontalalignment='center',verticalalignment='center',fontsize=8)
+
+plt.savefig(BASE_DIR+'masterThesis_analysis/figures/dados_observados/cebimar_salinidade.eps')
