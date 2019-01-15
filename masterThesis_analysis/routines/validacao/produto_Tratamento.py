b7238e3d19628d282fea31c8df7de4d5b1316ae3
Danilo Augusto Silva <nilodna@gmail.com>
2019-01-15 15:59:09 -0200

    new validation routine and location - CSS

---
+++ masterThesis_analysis/routines/validacao/produto_Tratamento.py
@@ -0,0 +1,90 @@
+# Le o arquivo de saida do modelo, faz tratamento e deixa pronto para validacao
+
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import pandas as pd
+import os
+import pickle
+from scipy.interpolate import griddata
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import dates
+import datetime
+import cmocean as cmo
+
+import decomp as dp
+
+import matplotlib
+matplotlib.style.use('ggplot')
+
+import sys
+sys.path.append('masterThesisPack/')
+
+import masterThesisPack as oceano
+
+##############################################################################
+#                          [GEN] FUNCTIONS                                   #
+##############################################################################
+# insert functions here
+
+
+##############################################################################
+#                               MAIN CODE                                    #
+##############################################################################
+# beginnig of the main code
+BASE_DIR = oceano.make_dir()
+DATA_DIR = BASE_DIR.replace('github','ventopcse/output')
+SAVE_DIR = BASE_DIR.replace('github','ventopcse')
+experiment = 'EA1.cdf'
+ncin = xr.open_dataset(DATA_DIR + experiment)
+
+# extracting data
+k5 = 6
+k15= 18
+
+u5 = ncin.u[:,k5,56,16].values
+v5 = ncin.v[:,k5,56,16].values
+u15= ncin.u[:,k15,56,16].values
+v15= ncin.u[:,k15,56,16].values
+
+dct = {
+    'u5' : u5,
+    'v5' : v5,
+    'u15' : u15,
+    'v15' : v15
+}
+
+dfModel = pd.DataFrame(dct,index=pd.DatetimeIndex(ncin.time.values))
+
+# rotation of 51 degrees
+alpha = np.deg2rad(51)
+# for 5m depth first
+INT,DIR = dp.uv2intdir(u5,v5,0,0)
+ur,vr = dp.intdir2uv(INT,DIR,0,alpha)
+
+df5m = pd.DataFrame({'along 5m':vr,'cross 5m':ur},index=dfModel.index)
+
+# now for 15m depth
+INT,DIR = dp.uv2intdir(u15,v15,0,0)
+ur,vr = dp.intdir2uv(INT,DIR,0,alpha)
+
+df15m = pd.DataFrame({'along 15m':vr,'cross 15m':ur},index=dfModel.index)
+
+
+# saving dataframes
+df5m = df5m.to_xarray()
+df5m.to_netcdf(SAVE_DIR + 'df5m.nc')
+df15m = df15m.to_xarray()
+df15m.to_netcdf(SAVE_DIR + 'df15m.nc')
+
+# for temperature
+T5 = ncin.temp[:,k5,56,16].values
+T15= ncin.temp[:,k15,56,16].values
+
+
+dfTemp = pd.DataFrame({'T5':T5,'T15':T15},index=pd.DatetimeIndex(ncin.time.values))
+dfTemp = dfTemp.to_xarray()
+dfTemp.to_netcdf(SAVE_DIR + 'dfTemp.nc')
---
+++ masterThesis_analysis/routines/validacao/validacao_CSS.py
@@ -0,0 +1,134 @@
+# add some description here
+
+import glob
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import numpy as np
+import pandas as pd
+import xarray as xr
+import pandas as pd
+import os
+import pickle
+from scipy.interpolate import griddata
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import dates
+import datetime
+import cmocean as cmo
+import scipy.io as sio
+
+import matplotlib
+matplotlib.style.use('ggplot')
+matplotlib.use('PS')
+
+import sys
+sys.path.append('masterThesisPack/')
+
+import masterThesisPack as oceano
+
+##############################################################################
+#                          [GEN] FUNCTIONS                                   #
+##############################################################################
+# insert functions here
+
+##############################################################################
+#                               MAIN CODE                                    #
+##############################################################################
+# beginnig of the main code
+BASE_DIR = oceano.make_dir()
+DATA_DIR = BASE_DIR.replace('github','ventopcse/data/IOUSP')
+SAVE_DIR = BASE_DIR.replace('github','ventopcse')
+FIG_DIR  = BASE_DIR + 'masterThesis_analysis/figures/validacao/'
+
+##################### IMPORTING OBSERVATIONAL DATA
+# reading file with CSS filtered data
+dfFilt = xr.open_dataset(SAVE_DIR + "CSS_filtered.nc")
+dfFilt = dfFilt.to_dataframe()
+# new datetimeIndex
+dtRange = pd.date_range(start='2013-12-02 21:00',end='2014-03-06 19:00',freq='30Min')
+dfFilt.index = dtRange
+
+# cutting and resampling insitu data
+dfTmp = dfFilt['2014-01-09 01:30:00':'2014-03-01 22:30:00'].copy()
+dfObse = dfTmp.resample('3h').mean()
+del dfTmp
+
+##################### IMPORTING MODELED DATA
+# importing product data, alread rotated and saved in a netcdf file
+df5m = xr.open_dataset(SAVE_DIR + 'df5m.nc')
+df5m = df5m.to_dataframe()
+df15m = xr.open_dataset(SAVE_DIR + 'df15m.nc')
+df15m = df15m.to_dataframe()
+dfTemp = xr.open_dataset(SAVE_DIR + 'dfTemp.nc')
+dfTemp = dfTemp.to_dataframe()
+
+# putting all data in the same dataframe
+dct = {
+    'u5m': df5m['along 5m'].values,
+    'v5m': df5m['cross 5m'].values,
+    'u15m': df15m['along 15m'].values,
+    'v15m': df15m['cross 15m'].values,
+    'T5m': dfTemp['T5'].values,
+    'T15m': dfTemp['T15'].values
+}
+
+dfModel = pd.DataFrame(dct,index=pd.date_range(start='2014-01-09 01:30:00',end='2014-03-01 22:30:00',freq='3h'))
+
+# corrigindo a data do modelo, que está em GMT
+time_data    = dfModel.index
+time_product = time_data - pd.Timedelta(hours=3)
+
+dfModel.index = time_product
+
+# calculating skill coefficient
+skillu5m  = oceano.skill_willmott(dfObse.u5m.values,dfModel.u5m.values)
+skillv5m  = oceano.skill_willmott(dfObse.v5m.values,dfModel.v5m.values)
+skillu15m = oceano.skill_willmott(dfObse.u15m.values,dfModel.u15m.values)
+skillv15m = oceano.skill_willmott(dfObse.v15m.values,dfModel.v15m.values)
+
+fig,axes = plt.subplots(nrows=4,sharex=True,figsize=(12./2.54,10/2.54))
+
+axes[0].set_title('Componente Perpendicular - Prof. de 5m (Skill %0.2f)'%(skillu5m),fontsize=8)
+axes[0].plot(dfObse.index.values,dfObse.u5m.values)
+axes[0].plot(dfModel.index.values,dfModel.u5m.values)
+axes[0].set_ylabel('u ['+r'$m^2$'+' s]',fontsize=8,labelpad=-2)
+axes[0].legend(['Observado','Modelado'],fontsize=6,bbox_to_anchor=(0.,-0.1,.4,.5),mode='expand',ncol=2)
+
+axes[1].set_title('Componente Paralela - Prof. de 5m (Skill %0.2f)'%(skillv5m),fontsize=8)
+axes[1].plot(dfObse.index.values,dfObse.v5m.values)
+axes[1].plot(dfModel.index.values,dfModel.v5m.values)
+axes[1].set_ylabel('v ['+r'$m^2$'+' s]',fontsize=8,labelpad=-2)
+axes[1].legend(['Observado','Modelado'],fontsize=6,bbox_to_anchor=(0.,-0.1,.4,.5),mode='expand',ncol=2)
+
+axes[2].set_title('Componente Perpendicular - Prof. de 15m (Skill %0.2f)'%(skillu15m),fontsize=8)
+axes[2].plot(dfObse.index.values,dfObse.u15m.values)
+axes[2].plot(dfModel.index.values,dfModel.u15m.values)
+axes[2].set_ylabel('u ['+r'$m^2$'+' s]',fontsize=8,labelpad=-2)
+axes[2].legend(['Observado','Modelado'],fontsize=6,bbox_to_anchor=(0.,-0.1,.4,.5),mode='expand',ncol=2)
+
+axes[3].set_title('Componente Paralela - Prof. de 15m (Skill %0.2f)'%(skillv15m),fontsize=8)
+axes[3].plot(dfObse.index.values,dfObse.v15m.values)
+axes[3].plot(dfModel.index.values,dfModel.v15m.values)
+axes[3].set_ylabel('v ['+r'$m^2$'+' s]',fontsize=8,labelpad=-2)
+axes[3].legend(['Observado','Modelado'],fontsize=6,bbox_to_anchor=(0.,-0.1,.4,.5),mode='expand',ncol=2)
+
+# configuring figure
+axes[0].set_ylim([-1.0,1.0])
+axes[1].set_ylim([-1.0,1.0])
+axes[2].set_ylim([-.5,.5])
+axes[3].set_ylim([-.5,.5])
+
+for ax in axes:
+    ax.axes.tick_params(axis='both',which='both',labelsize=8)
+    ax.margins(xmargin=0)
+
+# automatic formatting, for xdate
+fig.autofmt_xdate()
+
+axes[-1].xaxis.set_major_locator(mdates.WeekdayLocator())
+axes[-1].xaxis.set_major_formatter(mdates.DateFormatter('%d %b'))
+
+plt.tight_layout()
+plt.subplots_adjust(top=0.945,bottom=0.11,left=0.126,right=0.976,hspace=0.587,wspace=0.2)
+
+plt.savefig(FIG_DIR + 'css_validacao_2014.pdf')
