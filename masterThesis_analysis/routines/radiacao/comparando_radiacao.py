cfa84146fd875e8aae94c6e0054a932cd63f4ab7
Danilo Augusto Silva <nilodna@gmail.com>
2019-01-16 17:45:51 -0200

    comparing radiation

---
+++ masterThesis_analysis/routines/radiacao/comparando_radiacao.py
@@ -0,0 +1,46 @@
+# add some description here
+
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import pandas as pd
+import os
+import pickle
+from scipy.interpolate import griddata
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import dates
+import datetime
+import cmocean as cmo
+
+import matplotlib
+matplotlib.style.use('ggplot')
+
+import sys
+sys.path.append('masterThesisPack/')
+
+import masterThesisPack as oceano
+
+##############################################################################
+#                          [GEN] FUNCTIONS                                   #
+##############################################################################
+# insert functions here
+
+
+##############################################################################
+#                               MAIN CODE                                    #
+##############################################################################
+# beginnig of the main code
+BASE_DIR = oceano.make_dir()
+
+ncin = xr.open_dataset(BASE_DIR+'masterThesis_analysis/routines/radiacao/radiacao.nc')
+df   = ncin.to_dataframe()
+
+ts=[]
+for t in df.index.values:
+    ts.append(t[0])
+df.index = ts
+
+# importar dados de radiacao do CFSv2
