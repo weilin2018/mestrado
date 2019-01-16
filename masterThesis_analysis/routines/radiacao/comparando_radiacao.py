af4ee1f0b93cd7ee54469b26e309feb5f8f90872
Danilo Augusto Silva <nilodna@gmail.com>
2019-01-16 18:14:52 -0200

    inserting CFSv2 incoming shortwave radiation

--- masterThesis_analysis/routines/radiacao/comparando_radiacao.py
+++ masterThesis_analysis/routines/radiacao/comparando_radiacao.py
@@ -34,6 +34,7 @@ import masterThesisPack as oceano
 ##############################################################################
 # beginnig of the main code
 BASE_DIR = oceano.make_dir()
+DATA_DIR = BASE_DIR.replace('github','ventopcse/data/radiacao_ondacurta')
 
 ncin = xr.open_dataset(BASE_DIR+'masterThesis_analysis/routines/radiacao/radiacao.nc')
 df   = ncin.to_dataframe()
@@ -44,3 +45,22 @@ for t in df.index.values:
 df.index = ts
 
 # importar dados de radiacao do CFSv2
+ncin = xr.open_dataset(DATA_DIR+"ubatuba.nc")
+cfsv2 = pd.DataFrame({'UBATUBA_reanalise':np.squeeze(ncin.DSWRF_L1.values)},index=pd.DatetimeIndex(np.squeeze(ncin.time.values)))
+
+ncin = xr.open_dataset(DATA_DIR+"paraty.nc")
+cfsv2['PARATY_reanalise'] = np.squeeze(ncin.DSWRF_L1.values)
+
+# selecionando os dados
+reanalise = cfsv2['2013':'2017'].copy()
+# reamostrando dados observados para bater com os dados de reanalise
+observado = df.resample('6H').mean()
+
+# plotando dados para cada localizacao, mas passando um filtro quinzenal pra facilitar
+fig,ax = plt.subplots(nrows=2)
+
+reanalise.UBATUBA_reanalise.resample('D').mean().plot(ax=ax[0])
+observado.UBATUBA_rad.resample('D').mean().plot(ax=ax[0])
+
+reanalise.PARATY_reanalise.resample('D').mean().plot(ax=ax[1])
+observado.PARATY_rad.resample('D').mean().plot(ax=ax[1])
