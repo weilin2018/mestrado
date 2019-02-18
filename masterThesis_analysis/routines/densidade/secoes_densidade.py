7547915eaa8da4a41b639570ddbf57908654b97e
Danilo Augusto Silva <nilodna@gmail.com>
2019-02-18 18:32:41 -0300

    creating density and elevation directories

---
+++ masterThesis_analysis/routines/densidade/secoes_densidade.py
@@ -0,0 +1,275 @@
+#-*-coding;utf-8-*-
+"""
+
+"""
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import pandas as pd
+import os
+import pickle
+from scipy.interpolate import griddata
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import dates
+import datetime
+import cmocean as cmo
+import seawater as sw
+
+# pacotes para minimap
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
+import matplotlib
+# matplotlib.style.use('ggplot')
+matplotlib.use('PS')
+
+import sys
+sys.path.append('masterThesisPack/')
+
+import masterThesisPack as oceano
+
+##############################################################################
+#                          [GEN] FUNCTIONS                                   #
+##############################################################################
+# insert functions here
+def create_Structure_3(ncin,indexes):
+
+    fig,axes = plt.subplots(nrows=3,ncols=3,figsize=(25.4/2.54, 20/2.54))
+
+    for ind in indexes:
+        if ind == 99:
+            axesInd = 0
+            axes[axesInd,0].set_title('Climatologia',fontsize=8)
+            axes[axesInd,1].set_title(u'EA1 - 15 de Fevereiro',fontsize=8)
+            axes[axesInd,2].set_title(u'EA2 - 15 de Fevereiro',fontsize=8)
+        if ind == 28:
+            axesInd = 1
+        if ind == 19:
+            axesInd = 2
+            axes[axesInd,0].set_xlabel(u'Distância [km]',fontsize=8)
+            axes[axesInd,1].set_xlabel(u'Distância [km]',fontsize=8)
+            axes[axesInd,2].set_xlabel(u'Distância [km]',fontsize=8)
+
+    axes[0,0].set_ylabel('Profundidade [m]')
+    axes[1,0].set_ylabel('Profundidade [m]')
+    axes[2,0].set_ylabel('Profundidade [m]')
+
+    # hiding ticks labels
+    axes[0,0].xaxis.set_major_formatter(plt.NullFormatter())
+
+    axes[0,1].xaxis.set_major_formatter(plt.NullFormatter())
+    axes[0,1].yaxis.set_major_formatter(plt.NullFormatter())
+
+    axes[0,2].xaxis.set_major_formatter(plt.NullFormatter())
+    axes[0,2].yaxis.set_major_formatter(plt.NullFormatter())
+
+    axes[1,0].xaxis.set_major_formatter(plt.NullFormatter())
+
+    axes[1,1].xaxis.set_major_formatter(plt.NullFormatter())
+    axes[1,1].yaxis.set_major_formatter(plt.NullFormatter())
+
+    axes[1,2].xaxis.set_major_formatter(plt.NullFormatter())
+    axes[1,2].yaxis.set_major_formatter(plt.NullFormatter())
+
+    axes[2,1].yaxis.set_major_formatter(plt.NullFormatter())
+    axes[2,2].yaxis.set_major_formatter(plt.NullFormatter())
+
+    # adding colorbar axes (cbaxes)
+    cax = fig.add_axes([.3,.04,.45,.02])
+
+    return fig,axes,cax
+
+
+def search_information(ncin,ind,nstepBegin,nstepFinal,loc,var):
+    # based on an ind value, return a dictionary with informations for each
+    # cross section, such as location, variable position, etc.
+
+    # set some variables
+    if var == 'temp':
+        sigma = -1
+        value = 18.
+    if var == 'salt':
+        sigma = 0
+        value = 36.
+
+    # iniatilize dictionary
+    idx_begin = oceano.find_distance_of_a_value(ncin,ind,nstepBegin,sigma,var,value)[1][0][0]
+    idx_final = oceano.find_distance_of_a_value(ncin,ind,nstepFinal,sigma,var,value)[1][0][0]
+
+    info = {
+        'location': loc,
+        'beginPos_X': oceano.find_distance_of_a_value(ncin,ind,nstepBegin,sigma,var,value)[0],
+        'finalPos_X': oceano.find_distance_of_a_value(ncin,ind,nstepFinal,sigma,var,value)[0],
+        'beginPos_Z': oceano.find_depth_of_a_value(ncin,ind,nstepBegin,idx_begin,sigma,var,value),
+        'finalPos_Z': oceano.find_depth_of_a_value(ncin,ind,nstepFinal,idx_final,sigma,var,value)
+    }
+
+    return info
+
+##############################################################################
+#                               MAIN CODE                                    #
+##############################################################################
+# beginnig of the main code
+BASE_DIR = oceano.make_dir()
+plt.ion()
+
+exp = 'EA1'
+
+DATA_DIR = BASE_DIR.replace('github/', 'ventopcse/output/')
+fname = DATA_DIR + exp + '.cdf'
+
+ncin = xr.open_dataset(fname)
+
+lon,lat = ncin['lon'].values, ncin['lat'].values
+lon[lon == 0.] = np.nan
+lat[lat == 0.] = np.nan
+depth = ncin['depth'].values
+sigma = ncin['sigma'].values
+h1    = ncin['h1'].values
+
+# configurações do plot
+figsize = (17.4/2.54, 10/2.54)
+# index para as latitudes das seções
+indexes = [99,28,19]
+# configuracoes para as secoes verticais
+horizResolution = 10000
+vertResolution  = 100 # isso significa que teremos uma resolucao vertical de 1m
+depRef          = 200 # profundidade de referencia para interpolacao
+
+limiteEixoX = 300000 # limite, em metros, do eixo X para a secao vertical
+
+# defining the begin and the end to plot
+nstepBegin = np.arange(0,4,1) # begin of anomalous period
+nstepFinal = np.arange(297,303,1) # final of anomalous period
+
+fig,axes,caxes = create_Structure_3(ncin,indexes)
+
+title = u'Seção vertical de temperatura em Ubatuba (superior), Santos (meio) e Cananéia (inferior),\n'\
+      + u'com a estrutura climatológica à esquerda e a estrutura no dia 15 de Fevereiro em EA1 (meio) e EA2 (direita).\n'
+plt.suptitle(title,fontsize=10)
+
+for ind in indexes:
+    if ind == 99:
+        axesInd = 0
+    if ind == 28:
+        axesInd = 1
+    if ind == 19:
+        axesInd = 2
+
+    T = np.nanmean(ncin.temp[nstepBegin,:,ind,:],axis=0)
+    Tplot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,T,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
+
+    S = np.nanmean(ncin.salt[nstepBegin,:,ind,:],axis=0)
+    Splot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,S,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
+
+    # depth array
+    d = np.tile(ndepth,(10000,1)).T
+    # density 2D array
+    dens = sw.dens(Splot,Tplot,d)
+
+    contours = np.arange(1022,1028,0.1) # contours
+
+    # begin: 18 isotherm position
+    cf1  = axes[axesInd,0].contourf(xgrid,-zgrid,dens,contours,cmap=cmo.cm.dense,extend='both')
+    # cs   = axes[axesInd,0].contour(xgrid,-zgrid,Tplot,levels=[18.],colors=('k'),linestyles=('--'))
+    axes[axesInd,0].fill_between(dist2[-1,:], -depRef, sig[-1,:],color='#c0c0c0')
+    axes[axesInd,0].plot(dist2[-1,:],sig[-1,:],'k')
+    axes[axesInd,0].set_xlim([0,limiteEixoX])
+    axes[axesInd,0].set_ylim([-depRef,0])
+
+    T = np.nanmean(ncin.temp[nstepFinal,:,ind,:],axis=0)
+    Tplot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,T,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
+
+    S = np.nanmean(ncin.salt[nstepFinal,:,ind,:],axis=0)
+    Splot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,S,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
+
+    # density 2D array
+    dens = sw.dens(Splot,Tplot,d)
+
+    # final 18 isotherm position
+    cf2  = axes[axesInd,1].contourf(xgrid,-zgrid,dens,contours,cmap=cmo.cm.dense,extend='both')
+    # cs   = axes[axesInd,1].contour(xgrid,-zgrid,Tplot,levels=[18.],colors=('k'),linestyles=('--'))
+    axes[axesInd,1].fill_between(dist2[-1,:], -depRef, sig[-1,:],color='#c0c0c0')
+    axes[axesInd,1].plot(dist2[-1,:],sig[-1,:],'k')
+    axes[axesInd,1].set_xlim([0,limiteEixoX])
+    axes[axesInd,1].set_ylim([-depRef,1])
+
+    for c in cf1.collections:
+        c.set_edgecolor('face')
+        c.set_linewidth(0.00000000001)
+
+    for c in cf2.collections:
+        c.set_edgecolor('face')
+        c.set_linewidth(0.00000000001)
+
+######## Experimento Anomalo 2!!!!
+ncin = xr.open_dataset(fname.replace('1','2'))
+
+for ind in indexes:
+    if ind == 99:
+        axesInd = 0
+    if ind == 28:
+        axesInd = 1
+    if ind == 19:
+        axesInd = 2
+
+    T = np.nanmean(ncin.temp[nstepFinal,:,ind,:],axis=0)
+    Tplot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,T,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
+
+    S = np.nanmean(ncin.salt[nstepFinal,:,ind,:],axis=0)
+    Splot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,S,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
+
+    # density 2D array
+    dens = sw.dens(Splot,Tplot,d)
+
+    # final 18 isotherm position
+    cf3  = axes[axesInd,2].contourf(xgrid,-zgrid,dens,contours,cmap=cmo.cm.dense,extend='both')
+    axes[axesInd,2].fill_between(dist2[-1,:], -depRef, sig[-1,:],color='#c0c0c0')
+    axes[axesInd,2].plot(dist2[-1,:],sig[-1,:],'k')
+    axes[axesInd,2].set_xlim([0,limiteEixoX])
+    axes[axesInd,2].set_ylim([-depRef,1])
+
+    for c in cf3.collections:
+        c.set_edgecolor('face')
+        c.set_linewidth(0.00000000001)
+
+
+# plotting colorbar
+cbar = plt.colorbar(cf1,orientation='horizontal',cax=caxes,format='%.2f')
+# setting colorbar tick labels
+from matplotlib import ticker
+tick_locator = ticker.MaxNLocator(nbins=4)
+cbar.locator = tick_locator
+cbar.update_ticks()
+
+cbar.ax.axes.tick_params(axis='both',which='both',labelsize=8)
+cbar.ax.set_title(r'Densidade (kg m$^{-3}$)',fontsize=8)
+
+for c in cbar.ax.collections:
+    c.set_edgecolor('face')
+    c.set_linewidth(0.00000000001)
+
+
+# ajusta a figura antes de se ajustar os labels do eixo x, pois aumenta-se a quantidade de
+# ticks no eixo quando dá tight_layout
+plt.tight_layout()
+# plt.subplots_adjust(top=0.905,bottom=0.059,left=0.073,right=0.987,hspace=0.11,wspace=0.068)
+# plt.subplots_adjust(top=0.925,bottom=0.06,left=0.115,right=0.95,hspace=0.2,wspace=0.28)
+plt.subplots_adjust(top=0.900,bottom=0.144,left=0.073,right=0.987,hspace=0.075,wspace=0.058)
+
+# updating x tick labels
+labels = [item.get_text() for item in axes[2,0].get_xticklabels()]
+newlabels = []
+for lab in labels:
+    l = float(lab)/1000
+    newlabels.append(int(l))
+
+axes[2,0].set_xticklabels(newlabels)
+axes[2,1].set_xticklabels(newlabels)
+axes[2,2].set_xticklabels(newlabels)
+
+# plt.savefig(BASE_DIR.replace('mestrado/github','Dropbox/mestrado/figuras')+ 'secao_3x3plots.eps')
+# plt.savefig('/home/danilo/Dropbox/mestrado/figuras/secoes_verticais/evolucao_temp_3x3.pdf')
---
+++ masterThesis_analysis/routines/elevacao/campo_elevacao.py
@@ -0,0 +1,141 @@
+#-*-coding:utf-8-*-
+# add some description here
+
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import pandas as pd
+import os
+import pickle
+from scipy.interpolate import griddata
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import dates
+import datetime
+import cmocean as cmo
+import matplotlib.gridspec as gridspec
+
+import matplotlib
+# matplotlib.style.use('ggplot')
+matplotlib.use('PS')
+
+import sys
+sys.path.append('masterThesisPack/')
+
+import masterThesisPack as oceano
+
+##############################################################################
+#                          [GEN] FUNCTIONS                                   #
+##############################################################################
+# insert functions here
+def make_map(ax,llat=-30,ulat=-20,llon=-50,ulon=-39,resolution='l',nmeridians=3,nparallels=2,labels=[True,False,False,True]):
+    # criar mapa sem continente colorido, somente a linha de costa
+    # nao colocar meridianos e paralelos
+    # nao colocar coordenadas no eixo (sumir com os eixos)
+
+    m = Basemap(projection='merc',llcrnrlat=llat, urcrnrlat=ulat, llcrnrlon=llon, urcrnrlon=ulon, resolution='h')
+    # m = pickle.load(open('pickles/basemap.p','r'))
+    m.ax = ax
+    m.drawcoastlines(linewidth=.2)
+    m.fillcontinents(color='white',alpha=0)
+
+    meridians=np.arange(llon,ulon,nmeridians)
+    parallels=np.arange(llat,ulat,nparallels)
+
+    return m,meridians,parallels
+
+def export_data(fname,timestep=0):
+    # plotting climatologic data: t = 0, k = 0
+    ncin = xr.open_dataset(fname)
+
+    lon,lat = ncin.lon.values, ncin.lat.values
+    depth = ncin.depth.values
+    sigma = ncin.sigma.values
+    lon[lon == 0.] = np.nan
+    lat[lat == 0.] = np.nan
+    depth = ncin.depth.values
+
+    # extracting salinity data, in a specific timestep
+    elev = ncin.elev[timestep,:,:]
+    elev = np.where(depth < 100, elev,np.nan)
+
+    return lon,lat,elev,depth
+
+def configurePlot(axes):
+
+    axes[0,0].set_title(u'EC1 - 14/Janeiro',fontsize=8)
+    axes[1,0].set_title(u'EC2 - 15/Fevereiro',fontsize=8)
+    axes[0,1].set_title(u'EA1 - 14/Janeiro',fontsize=8)
+    axes[1,1].set_title(u'EA2 - 15/Fevereiro',fontsize=8)
+
+##############################################################################
+#                               MAIN CODE                                    #
+##############################################################################
+# beginnig of the main code
+BASE_DIR = oceano.make_dir()
+DATA_DIR = BASE_DIR.replace('github/', 'ventopcse/output/')
+fname = DATA_DIR + 'EA1.cdf'
+
+plt.ion()
+
+fig,axes = plt.subplots(ncols=2,nrows=2)
+configurePlot(axes)
+timestep = [46,303]
+contours = np.arange(-.5,.5,0.01)
+cont = 0 # axis to plot
+
+# plot EA1
+### begin
+lon,lat,elev,depth = export_data(fname,timestep=46)
+elev[:5,:] = np.nan
+elev[-5:,:] = np.nan
+
+m,meridians,parallels = make_map(axes[0,0],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+
+cf = m.contourf(lon,lat,elev,contours,cmap='RdBu',latlon=True,rasterized=True)
+
+for c in cf.collections:
+    c.set_edgecolor('face')
+    c.set_linewidth(0.00000000001)
+
+### final
+lon,lat,elev,depth = export_data(fname,timestep=303)
+elev[:5,:] = np.nan
+elev[-5:,:] = np.nan
+
+m,meridians,parallels = make_map(axes[1,0],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+
+cf = m.contourf(lon,lat,elev,contours,cmap='RdBu',latlon=True,rasterized=True)
+
+for c in cf.collections:
+    c.set_edgecolor('face')
+    c.set_linewidth(0.00000000001)
+
+# plot EA2
+### begin
+lon,lat,elev,depth = export_data(fname.replace('1','2'),timestep=46)
+elev[:5,:] = np.nan
+elev[-5:,:] = np.nan
+
+m,meridians,parallels = make_map(axes[0,1],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+
+cf = m.contourf(lon,lat,elev,contours,cmap='RdBu',latlon=True,rasterized=True)
+
+for c in cf.collections:
+    c.set_edgecolor('face')
+    c.set_linewidth(0.00000000001)
+
+### final
+lon,lat,elev,depth = export_data(fname.replace('1','2'),timestep=303)
+elev[:5,:] = np.nan
+elev[-5:,:] = np.nan
+
+m,meridians,parallels = make_map(axes[1,1],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+
+cf = m.contourf(lon,lat,elev,contours,cmap='RdBu',latlon=True,rasterized=True)
+
+for c in cf.collections:
+    c.set_edgecolor('face')
+    c.set_linewidth(0.00000000001)
