dd36ddb66d6e2a87cf3e4f32d4e27a4d36486b0a
Danilo A. Silva <nilodna@gmail.com>
2019-03-22 10:35:23 -0300

    change in velocity field

--- masterThesis_analysis/routines/velocidade/campo_medio.py
+++ masterThesis_analysis/routines/velocidade/campo_medio.py
@@ -92,6 +92,111 @@ def calculateMeanvelocity(u,v,depth,angles):
 
     return umean_norm,vmean_norm,smean
 
+
+def plot_meandepth(fname):
+
+    # selecting the range of timesteps to calculate a daily mean and then a depth average
+    nstepClim  = np.arange(0,2,1)
+    nstepBegin = np.arange(48,57,1)
+    nstepFinal = np.arange(280,289,1)
+
+    # importing general variables to create figures
+    ncin = xr.open_dataset(fname)
+    ncin2 = xr.open_dataset(fname.replace('C','A'))
+
+    lon,lat = ncin.lon.values.copy(), ncin.lat.values.copy()
+    angle = ncin.ang.values.copy()
+    depth = ncin.depth.values.copy()
+    sigma = ncin.sigma.values.copy()
+    lon[lon == 0.] = np.nan
+    lat[lat == 0.] = np.nan
+
+    fig,axes = plt.subplots(nrows=3,ncols=2,figsize=(18.4/2.54, 26/2.54))
+    cax = fig.add_axes([.275,.04,.45,.02])
+
+    axes[0,0].set_title('%s - Climatologia'%(experiment),fontsize=8)
+    axes[0,1].set_title('%s - Climatologia'%(experiment),fontsize=8)
+    axes[1,0].set_title('%s - 15/Jan'%(experiment),fontsize=8)
+    axes[1,1].set_title('%s - 13/Fev'%(experiment),fontsize=8)
+    axes[2,0].set_title('%s - 15/Jan'%(experiment.replace('C','A')),fontsize=8)
+    axes[2,1].set_title('%s - 13/Fev'%(experiment.replace('C','A')),fontsize=8)
+
+    m00,_,_ = make_map(axes[0,0],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+    m01,_,_ = make_map(axes[0,1],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+
+    m1,meridians,parallels = make_map(axes[1,0],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+    m2,_,_ = make_map(axes[1,1],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+    m3,_,_ = make_map(axes[2,0],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+    m4,_,_ = make_map(axes[2,1],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
+
+    # plot climatology of EC1
+    u = np.nanmean(ncin.u[nstepClim,:,:,:].values,axis=0)
+    v = np.nanmean(ncin.v[nstepClim,:,:,:].values,axis=0)
+    umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
+    xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
+    cf1 = m00.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
+    qv1 = m00.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
+
+    # plot climatology of EA1
+    u = np.nanmean(ncin2.u[nstepClim,:,:,:].values,axis=0)
+    v = np.nanmean(ncin2.v[nstepClim,:,:,:].values,axis=0)
+    umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
+    xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
+    cf1 = m01.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
+    qv1 = m01.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
+
+    # plotting EC1 - 14/Jan
+    u = np.nanmean(ncin.u[nstepBegin,:,:,:].values,axis=0)
+    v = np.nanmean(ncin.v[nstepBegin,:,:,:].values,axis=0)
+    umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
+    xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
+
+    cf1 = m1.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
+    qv1 = m1.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
+
+    # plotting EC1 - 15/Fev
+    u = np.nanmean(ncin.u[nstepFinal,:,:,:].values,axis=0)
+    v = np.nanmean(ncin.v[nstepFinal,:,:,:].values,axis=0)
+    umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
+    xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
+
+    cf2 = m2.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
+    qv2 = m2.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
+
+    # plotting EA1 - 14/Jan
+    u = np.nanmean(ncin2.u[nstepBegin,:,:,:].values,axis=0)
+    v = np.nanmean(ncin2.v[nstepBegin,:,:,:].values,axis=0)
+    umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
+    xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
+
+    cf3 = m3.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
+    qv3 = m3.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
+
+    # plotting EA1 - 15/Fev
+    u = np.nanmean(ncin2.u[nstepFinal,:,:,:].values,axis=0)
+    v = np.nanmean(ncin2.v[nstepFinal,:,:,:].values,axis=0)
+    umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
+    xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
+
+    cf4 = m4.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
+    qv4 = m4.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
+
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.969,bottom=0.088,left=0.021,right=0.979,hspace=0.081,wspace=0.0)
+
+
+    cbar = plt.colorbar(cf1,orientation='horizontal',cax=cax,format='%.2f')
+    # setting colorbar tick labels
+    from matplotlib import ticker
+    tick_locator = ticker.MaxNLocator(nbins=5)
+    cbar.locator = tick_locator
+    cbar.update_ticks()
+
+    cbar.ax.axes.tick_params(axis='both',which='both',labelsize=8)
+    cbar.ax.set_title(r'Velocidade (m.s$^{-1}$)',fontsize=8)
+
+    # plt.savefig('/home/danilo/Dropbox/mestrado/figuras/campo_medio_Z____%s.pdf'%(experiment))
+
 ##############################################################################
 #                               MAIN CODE                                    #
 ##############################################################################
@@ -104,106 +209,10 @@ experiment = raw_input('Digite o experimento controle a ser plotado [EC1, EC2]: 
 fname = DATA_DIR + experiment + '.cdf'
 plt.ion()
 
-# selecting the range of timesteps to calculate a daily mean and then a depth average
-nstepClim  = np.arange(0,2,1)
-nstepBegin = np.arange(48,57,1)
-nstepFinal = np.arange(280,289,1)
+mean = 'depth'
 
-# importing general variables to create figures
-ncin = xr.open_dataset(fname)
-ncin2 = xr.open_dataset(fname.replace('C','A'))
+if mean == 'depth':
+    plot_meandepth(fname)
 
-lon,lat = ncin.lon.values.copy(), ncin.lat.values.copy()
-angle = ncin.ang.values.copy()
-depth = ncin.depth.values.copy()
-sigma = ncin.sigma.values.copy()
-lon[lon == 0.] = np.nan
-lat[lat == 0.] = np.nan
-
-fig,axes = plt.subplots(nrows=3,ncols=2,figsize=(18.4/2.54, 26/2.54))
-cax = fig.add_axes([.275,.04,.45,.02])
-
-axes[0,0].set_title('%s - Climatologia'%(experiment),fontsize=8)
-axes[0,1].set_title('%s - Climatologia'%(experiment),fontsize=8)
-axes[1,0].set_title('%s - 15/Jan'%(experiment),fontsize=8)
-axes[1,1].set_title('%s - 13/Fev'%(experiment),fontsize=8)
-axes[2,0].set_title('%s - 15/Jan'%(experiment.replace('C','A')),fontsize=8)
-axes[2,1].set_title('%s - 13/Fev'%(experiment.replace('C','A')),fontsize=8)
-
-m00,_,_ = make_map(axes[0,0],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
-m01,_,_ = make_map(axes[0,1],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
-
-m1,meridians,parallels = make_map(axes[1,0],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
-m2,_,_ = make_map(axes[1,1],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
-m3,_,_ = make_map(axes[2,0],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
-m4,_,_ = make_map(axes[2,1],ulon=np.nanmax(lon)-.5,llon=np.nanmin(lon)-.2,ulat=np.nanmax(lat)+.2,llat=np.nanmin(lat))
-
-# plot climatology of EC1
-u = np.nanmean(ncin.u[nstepClim,:,:,:].values,axis=0)
-v = np.nanmean(ncin.v[nstepClim,:,:,:].values,axis=0)
-umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
-xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
-cf1 = m00.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
-qv1 = m00.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
-
-# plot climatology of EA1
-u = np.nanmean(ncin2.u[nstepClim,:,:,:].values,axis=0)
-v = np.nanmean(ncin2.v[nstepClim,:,:,:].values,axis=0)
-umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
-xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
-cf1 = m01.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
-qv1 = m01.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
-
-# plotting EC1 - 14/Jan
-u = np.nanmean(ncin.u[nstepBegin,:,:,:].values,axis=0)
-v = np.nanmean(ncin.v[nstepBegin,:,:,:].values,axis=0)
-umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
-xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
-
-cf1 = m1.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
-qv1 = m1.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
-
-# plotting EC1 - 15/Fev
-u = np.nanmean(ncin.u[nstepFinal,:,:,:].values,axis=0)
-v = np.nanmean(ncin.v[nstepFinal,:,:,:].values,axis=0)
-umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
-xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
-
-cf2 = m2.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
-qv2 = m2.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
-
-# plotting EA1 - 14/Jan
-u = np.nanmean(ncin2.u[nstepBegin,:,:,:].values,axis=0)
-v = np.nanmean(ncin2.v[nstepBegin,:,:,:].values,axis=0)
-umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
-xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
-
-cf3 = m3.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
-qv3 = m3.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
-
-# plotting EA1 - 15/Fev
-u = np.nanmean(ncin2.u[nstepFinal,:,:,:].values,axis=0)
-v = np.nanmean(ncin2.v[nstepFinal,:,:,:].values,axis=0)
-umean_norm,vmean_norm,smean = calculateMeanvelocity(u,v,depth,angle)
-xplot,yplot,uplot,vplot = ocplt.formatting_vectors(umean_norm,vmean_norm,lon,lat,FILE_DIR)
-
-cf4 = m4.contourf(lon,lat,smean,latlon=True,cmap=cmo.cm.speed,rasterized=True)
-qv4 = m4.quiver(xplot,yplot,uplot,vplot,scale=60,width=0.0015,headwidth=4,headlength=4,latlon=True)
-
-plt.tight_layout()
-plt.subplots_adjust(top=0.969,bottom=0.088,left=0.021,right=0.979,hspace=0.081,wspace=0.0)
-
-
-cbar = plt.colorbar(cf1,orientation='horizontal',cax=cax,format='%.2f')
-# setting colorbar tick labels
-from matplotlib import ticker
-tick_locator = ticker.MaxNLocator(nbins=5)
-cbar.locator = tick_locator
-cbar.update_ticks()
-
-cbar.ax.axes.tick_params(axis='both',which='both',labelsize=8)
-cbar.ax.set_title(r'Velocidade (m.s$^{-1}$)',fontsize=8)
-
-
-
-plt.savefig('/home/danilo/Dropbox/mestrado/figuras/campo_medio_Z____%s.pdf'%(experiment))
+if mean == 'temporal':
+    plot_meantemporal(fname)
--- masterThesis_analysis/routines/velocidade/secao_vertical_paralela.py
+++ masterThesis_analysis/routines/velocidade/secao_vertical_paralela.py
@@ -35,28 +35,26 @@ import masterThesisPack.plots as ocplot
 #                          [GEN] FUNCTIONS                                   #
 ##############################################################################
 # insert functions here
-def create_Structure(figsize):
+def create_structure_1row(indexes,exp):
 
-    fig,axes = plt.subplots(nrows=3,ncols=2,figsize=figsize)
+    fig,axes = plt.subplots(nrows=1,ncols=3,figsize=(20.4/2.54, 10/2.54))
 
-    axes[0,0].set_title('Perpendicular',fontsize=8)
-    axes[0,1].set_title('Paralela',fontsize=8)
+    axes[0].set_title(u'Seção Norte',fontsize=8)
+    axes[1].set_title(u'Seção Centro',fontsize=8)
+    axes[2].set_title(u'Seção Sul',fontsize=8)
 
-    axes[0,0].xaxis.set_major_formatter(plt.NullFormatter())
-    axes[0,1].xaxis.set_major_formatter(plt.NullFormatter())
-    axes[1,0].xaxis.set_major_formatter(plt.NullFormatter())
-    axes[1,1].xaxis.set_major_formatter(plt.NullFormatter())
-    axes[0,1].yaxis.set_major_formatter(plt.NullFormatter())
-    axes[1,1].yaxis.set_major_formatter(plt.NullFormatter())
-    axes[2,1].yaxis.set_major_formatter(plt.NullFormatter())
+    axes[0].set_xlabel(u'Distância [km]', fontsize=8)
+    axes[1].set_xlabel(u'Distância [km]', fontsize=8)
+    axes[2].set_xlabel(u'Distância [km]', fontsize=8)
 
-    axes[0,0].set_ylabel(u'Profundidade [m]',fontsize=8)
-    axes[1,0].set_ylabel(u'Profundidade [m]',fontsize=8)
-    axes[2,0].set_ylabel(u'Profundidade [m]',fontsize=8)
-    axes[2,0].set_xlabel(u'Distância [km]', fontsize=8)
-    axes[2,1].set_xlabel(u'Distância [km]', fontsize=8)
+    axes[0].set_ylabel(u'Profundidade [m]',fontsize=8)
 
-    cax = fig.add_axes([.3,.05,.45,.02])
+    # hiding ticks labels
+    axes[1].yaxis.set_major_formatter(plt.NullFormatter())
+    axes[2].yaxis.set_major_formatter(plt.NullFormatter())
+
+    # adding colorbar axes (cbaxes)
+    cax = fig.add_axes([.35,.05,.35,.02])
 
     return fig,axes,cax
 
@@ -106,8 +104,9 @@ depth = ncin['depth'].values
 sigma = ncin['sigma'].values
 h1    = ncin['h1'].values
 angle = ncin['ang'].values
+
 # configurações do plot
-figsize = (15.4/2.54, 18/2.54)
+figsize = (17.4/2.54, 10/2.54)
 # index para as latitudes das seções
 indexes = [99,28,19]
 # configuracoes para as secoes verticais
@@ -118,21 +117,17 @@ depRef          = 200 # profundidade de referencia para interpolacao
 limiteEixoX = 300000 # limite, em metros, do eixo X para a secao vertical
 contours = np.arange(-.4,.4,0.01)
 
-fig,axes,cax = create_Structure(figsize)
-title = u'Seção vertical de velocidade em Ubatuba (superior), Santos (meio) e \nCananéia (inferior),'\
-      + u'no dia 15 de Janeiro em %s.\n'% (exp)
+fig,axes,cax = create_structure_1row(indexes,exp)
+title = u'Seção vertical da componente paralela em Ubatuba (esquerda), Santos (meio) e Cananéia (direita),\n'\
+      + u'no dia 13 de Fevereiro em %s.\n'% (exp)
 plt.suptitle(title,fontsize=10)
 
-# figure adjustments
-plt.tight_layout()
-plt.subplots_adjust(top=0.892,bottom=0.158,left=0.113,right=0.979,hspace=0.075,wspace=0.085)
-
-nstepBegin = np.arange(48,57,1)   # first day
+nstepBegin = np.arange(0,9,1) # climatolofic data
 nstepFinal = np.arange(280,289,1) # final of anomalous period
 
 # rotating vectors
-u = np.nanmean(ncin.u[nstepBegin,:,:,:],axis=0)
-v = np.nanmean(ncin.v[nstepBegin,:,:,:],axis=0)
+u = np.nanmean(ncin.u[nstepFinal,:,:,:],axis=0)
+v = np.nanmean(ncin.v[nstepFinal,:,:,:],axis=0)
 ur,vr = np.zeros(u.shape)*np.nan,np.zeros(u.shape)*np.nan
 
 for t in range(u.shape[0]):
@@ -140,7 +135,6 @@ for t in range(u.shape[0]):
     ur[t,:,:] = urot
     vr[t,:,:] = vrot
 
-
 for ind in indexes:
     if ind == 99:
         axesInd = 0
@@ -149,42 +143,18 @@ for ind in indexes:
     if ind == 19:
         axesInd = 2
 
-    print('# ----- PLOTTING [secao: %i] 14 JAN, 2014 ----- #'%(ind))
-    U = ur[:,ind,:]
-    Tplot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,U,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
+    V = vr[:,ind,:]
+    Vplot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,V,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
     # gridding vertical section
     xgrid,zgrid = np.meshgrid(ndist,ndepth)
 
     # begin: 18 isotherm position
-    cf1  = axes[axesInd,0].contourf(xgrid,-zgrid,Tplot,contours,cmap=cmo.cm.delta,extend='both')
-    axes[axesInd,0].fill_between(dist2[-1,:], -depRef, sig[-1,:],color='#c0c0c0')
-    axes[axesInd,0].plot(dist2[-1,:],sig[-1,:],'k')
-    axes[axesInd,0].set_xlim([0,limiteEixoX
-    ])
-    axes[axesInd,0].set_ylim([-depRef,0])
-
-for ind in indexes:
-    if ind == 99:
-        axesInd = 0
-    if ind == 28:
-        axesInd = 1
-    if ind == 19:
-        axesInd = 2
-
-    print('# ----- PLOTTING [secao: %i] 14 JAN, 2014 ----- #'%(ind))
-    U = vr[:,ind,:]
-    Tplot,ndist,ndepth,dist2,sig,depth = oceano.crossSection_optimized(lon,depth,sigma,h1,U,horizResolution=horizResolution,vertResolution=vertResolution,depRef=depRef,ind=ind)
-    # gridding vertical section
-    xgrid,zgrid = np.meshgrid(ndist,ndepth)
-
-    # begin: 18 isotherm position
-    cf1  = axes[axesInd,1].contourf(xgrid,-zgrid,Tplot,contours,cmap=cmo.cm.delta,extend='both')
-    axes[axesInd,1].fill_between(dist2[-1,:], -depRef, sig[-1,:],color='#c0c0c0')
-    axes[axesInd,1].plot(dist2[-1,:],sig[-1,:],'k')
-    axes[axesInd,1].set_xlim([0,limiteEixoX
-    ])
-    axes[axesInd,1].set_ylim([-depRef,0])
-
+    cf1  = axes[axesInd].contourf(xgrid,-zgrid,Vplot,contours,cmap=cmo.cm.delta,extend='both')
+    # cs   = axes[axesInd].contour(xgrid,-zgrid,Vplot,levels=[18.],colors=('k'),linestyles=('--'))
+    axes[axesInd].fill_between(dist2[-1,:], -depRef, sig[-1,:],color='#c0c0c0')
+    axes[axesInd].plot(dist2[-1,:],sig[-1,:],'k')
+    axes[axesInd].set_xlim([0,limiteEixoX])
+    axes[axesInd].set_ylim([-depRef,0])
 
 # plotting colorbar
 cbar = plt.colorbar(cf1,orientation='horizontal',cax=cax,format='%.2f')
@@ -201,18 +171,24 @@ for c in cbar.ax.collections:
     c.set_edgecolor('face')
     c.set_linewidth(0.00000000001)
 
+# ajusta a figura antes de se ajustar os labels do eixo x, pois aumenta-se a quantidade de
+# ticks no eixo quando dá tight_layout
+plt.tight_layout()
+# plt.subplots_adjust(top=0.905,bottom=0.059,left=0.073,right=0.987,hspace=0.11,wspace=0.068)
+# plt.subplots_adjust(top=0.925,bottom=0.06,left=0.115,right=0.95,hspace=0.2,wspace=0.28)
+plt.subplots_adjust(top=0.841,bottom=0.237,left=0.087,right=0.982,hspace=0.099,wspace=0.118)
 
-# updating x tick labels
-labels = [item.get_text() for item in axes[2,0].get_xticklabels()]
+labels = [item.get_text() for item in axes[0].get_xticklabels()]
 newlabels = []
 for lab in labels:
     l = float(lab)/1000
     newlabels.append(int(l))
 
-axes[2,0].set_xticklabels(newlabels)
-axes[2,1].set_xticklabels(newlabels)
+axes[0].set_xticklabels(newlabels)
+axes[1].set_xticklabels(newlabels)
+axes[2].set_xticklabels(newlabels)
 
-plt.savefig('/home/danilo/Dropbox/mestrado/figuras/secoes_verticais/evolucao_speed_%s.eps'%(exp))
-plt.savefig('/home/danilo/Dropbox/mestrado/figuras/lowResolution/secoes_verticais/evolucao_speed_%s.png'%(exp))
+plt.savefig('/home/danilo/Dropbox/mestrado/figuras/secoes_verticais/secao_paralela_%s.eps'%(exp))
+plt.savefig('/home/danilo/Dropbox/mestrado/figuras/lowResolution/secoes_verticais/secao_paralela_%s.png'%(exp))
 plt.close()
 %reset -f
