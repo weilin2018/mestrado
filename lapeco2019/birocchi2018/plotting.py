da688f6f621661bb397118aa4a4c5528dd3156d0
Danilo Augusto Silva <nilodna@gmail.com>
2019-05-08 17:50:02 -0300

    create birocchi2018

---
+++ lapeco2019/birocchi2018/plotting.py
@@ -0,0 +1,31 @@
+#-*-coding;utf-8-*-
+"""
+
+"""
+import glob
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import xarray as xr
+import pandas as pd
+import os
+import pickle
+from scipy.interpolate import griddata
+from mpl_toolkits.basemap import Basemap
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib import dates
+import datetime
+import cmocean as cmo
+import gsw
+
+# pacotes para minimap
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+
+import matplotlib
+matplotlib.style.use('ggplot')
+
+import sys
+sys.path.append('/home/danilo/mestrado/github/masterThesis_analysis/routines/masterThesisPack/')
+
+import masterThesisPack as oceano
